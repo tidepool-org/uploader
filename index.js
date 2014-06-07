917263a17cfaab173dd6ed060997747f840e2aab
kentquirk <kentquirk@gmail.com>
2014-06-06 22:22:40 -0400

    It now lets you log in and fetch profile data.

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
 // buildUI.js
 // this constructs the UI in jQuery
 
+var make_base_auth = function (username, password) {
+  var tok = username + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+
+var tidepoolServerData = {
+    host: '',
+    usertoken: '',
+    userdata: null,
+    isLoggedIn: false,
+};
+
+var tidepoolServer = {
+    get: function(url, query, happycb, sadcb) {
+        jqxhr = $.ajax({
+            type: 'GET',
+            url: url,
+            headers: { 'x-tidepool-session-token': tidepoolServerData.usertoken }
+        }).success(function(data, status, jqxhr) {
+            tidepoolServerData.usertoken = jqxhr.getResponseHeader('x-tidepool-session-token');
+            happycb(data, status, jqxhr);
+        }).error(function(jqxhr, status, err) {
+            sadcb(jqxhr, status, err);
+        });
+    },
+    post: function(path, data, happycb, sadcb) {
+        jqxhr = $.post(url, query, happycb).fail(sadcb);
+    },
+    login: function(username, password, happycb, sadcb) {
+        var url = tidepoolServerData.host + "/auth/login";
+        jqxhr = $.ajax({
+            type: 'POST',
+            url: url,
+            headers: { "Authorization": make_base_auth(username, password) }, 
+        }).success(function(data, status, jqxhr) {
+            tidepoolServerData.usertoken = jqxhr.getResponseHeader('x-tidepool-session-token');
+            tidepoolServerData.userdata = data;
+            happycb(data, status, jqxhr);
+        }).error(function(jqxhr, status, err) {
+            sadcb(jqxhr, status, err);
+        });
+    },
+    getProfile: function(happycb, sadcb) {
+        var url = tidepoolServerData.host + "/metadata/" + tidepoolServerData.userdata.userid + "/profile";
+        this.get(url, null, happycb, sadcb);
+    }
+}
+
+
 function constructUI() {
     //$('body').append('This is a test.');
 
@@ -49,18 +99,43 @@ function constructUI() {
         var password = $('#password').val();
         var server = $('#serverURL').val();
         console.log(username, password, server);
-        $(".loginname").text(username);
-        loggedIn(true);
+        tidepoolServerData.host = server;
+
+        var goodLogin = function(data, status, jqxhr) {
+            console.log(data);
+            connectLog(status);
+            getProfile();
+            loggedIn(true);
+        }
+
+        var failLogin = function(jqxhr, status, error) {
+            connectLog("Login FAILED!", status, error);
+            loggedIn(false);
+        }
+
+        var goodProfile = function(data, status, jqxhr) {
+            connectLog(status);
+            connectLog(data.toString());
+            $(".loginname").text(data.fullName);
+        }
+
+        var failProfile = function(jqxhr, status, error) {
+            connectLog("FAILED!", status, error);
+        }
+
+        var getProfile = function() {
+            connectLog("Fetching profile.");
+            tidepoolServer.getProfile(goodProfile, failProfile);
+        };
+
+        tidepoolServer.login(username, password, goodLogin, failLogin);
     });
 
     $("#logoutButton").click(function() {
         loggedIn(false);
     });
 
-    $("#testButton").click(function() {
-        connectLog("Button clicked.");
-    });
-
+    $("#testButton").click(getProfile);
 }
 
 $(constructUI)
