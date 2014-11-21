40ca9ec32075148b891f6cf09635daaa842b02c4
Jamie <jamie@tidepool.org>
2014-11-21 12:03:19 -0800

    fix for deviceId not being set on suspendBasal

--- test/testObjectBuilder.js
+++ test/testObjectBuilder.js
@@ -157,6 +157,19 @@ describe('objectBuilder.js', function(){
       expect(suspendBasal.previous).to.equal(OPTIONAL);
 
     });
+
+    it('can use with_deviceId', function(){
+
+      var updatedDeviceId = '123';
+
+      var suspendBasal =
+        bob.makeSuspendBasal()
+        .with_deviceId(updatedDeviceId);
+
+      expect(suspendBasal.deviceId).to.equal(updatedDeviceId);
+
+    });
+
   });
 
 });
