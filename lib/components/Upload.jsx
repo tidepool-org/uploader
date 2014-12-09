f9245bfdba0d871a869995a00dc13773e5f4478c
ianjorgensen <jorgensen.ian@gmail.com>
2014-12-09 17:17:33 +0100

    fix carelink enabled

--- lib/components/Upload.jsx
+++ lib/components/Upload.jsx
@@ -30,6 +30,12 @@ var Upload = React.createClass({
     onUpload: React.PropTypes.func.isRequired
   },
 
+  getInitialState: function() {
+    return {
+      careLinkUploadDisabled: true
+    };
+  },
+
   render: function() {
     var classes = cx({
       'Upload': true,
@@ -90,10 +96,21 @@ var Upload = React.createClass({
 
     return (
       <div>
-        <div className="Upload-input"><input className="form-control" ref="username" placeholder="CareLink username"/></div>
-        <div className="Upload-input"><input className="form-control" ref="password" type="password" placeholder="CareLink password"/></div>
+        <div className="Upload-input"><input onChange={this.onCareLinkInputChange} className="form-control" ref="username" placeholder="CareLink username"/></div>
+        <div className="Upload-input"><input onChange={this.onCareLinkInputChange} className="form-control" ref="password" type="password" placeholder="CareLink password"/></div>
       </div>
     );
+  },
+
+  onCareLinkInputChange: function() {
+    var username = this.refs.username && this.refs.username.getDOMNode().value;
+    var password = this.refs.password && this.refs.password.getDOMNode().value;
+
+    if (!username || !password) {
+      this.setState({careLinkUploadDisabled: true});
+    } else {
+      this.setState({careLinkUploadDisabled: false});
+    }
   },
 
   renderButton: function() {
@@ -189,12 +206,7 @@ var Upload = React.createClass({
 
   isDisabled: function() {
     if (this.isCarelinkUpload()) {
-      var username = this.refs.username && this.refs.username.getDOMNode().value;
-      var password = this.refs.password && this.refs.password.getDOMNode().value;
-
-      if (!username || !password) {
-        //return true;
-      }
+      return this.state.careLinkUploadDisabled;
     }
 
     return this.props.upload.disabled;
