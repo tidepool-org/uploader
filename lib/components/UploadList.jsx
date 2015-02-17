31b2f0bb25243c1c654168bc81876a28d590ce1c
Jana E. Beck <jana.eliz.beck@gmail.com>
2015-02-16 19:24:53 -0800

    bugfixes for uploadIndex

--- lib/components/UploadList.jsx
+++ lib/components/UploadList.jsx
@@ -22,17 +22,22 @@ var Upload = require('./Upload.jsx');
 var UploadList = React.createClass({
   propTypes: {
     uploads: React.PropTypes.array.isRequired,
+    targetedUploads: React.PropTypes.array.isRequired,
     onUpload: React.PropTypes.func.isRequired,
-    onReset: React.PropTypes.func.isRequired
+    onReset: React.PropTypes.func.isRequired,
+    readFile: React.PropTypes.func.isRequired
   },
 
   render: function() {
     var self = this;
-    var nodes = _.map(this.props.uploads, function(upload, index){
+    var nodes = _.map(this.props.targetedUploads, function(target){
+      var index = _.findIndex(self.props.uploads, function(upload) {
+        return upload.key === target.key;
+      });
       return (
         <div key={index} className="UploadList-item">
           <Upload
-            upload={upload}
+            upload={target}
             onUpload={self.props.onUpload.bind(null, index)}
             onReset={self.props.onReset.bind(null, index)}
             readFile={self.props.readFile.bind(null, index)} />
