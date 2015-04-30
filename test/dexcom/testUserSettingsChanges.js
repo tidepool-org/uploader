1ce7aa6f3aac789ea8c8b40407e292816195bc94
Jana E. Beck <jana.eliz.beck@gmail.com>
2015-04-30 15:35:05 -0700

    basic tests for construction of timeChanges in Dexcom userSettingsChanges

--- test/dexcom/testUserSettingsChanges.js
+++ test/dexcom/testUserSettingsChanges.js
@@ -19,18 +19,77 @@
 
 var _ = require('lodash');
 var expect = require('salinity').expect;
+var sundial = require('sundial');
 
+var builder = require('../../lib/objectBuilder')();
 var userSettingsChanges = require('../../lib/dexcom/userSettingsChanges');
 
 describe('userSettingsChanges.js', function() {
-	it('is a function', function() {
-		expect(typeof userSettingsChanges).to.equal('function');
-	});
+  var settings = [
+    {
+      displayOffset: 0,
+      internalTime: '2009-01-10T06:19:55',
+      jsDate: new Date(Date.UTC(2009,0,1,0,0,0) + 1000*800395),
+      systemSeconds: 800395,
+      systemTimeMsec: 1231568395000
+    },
+    {
+      displayOffset: -28800,
+      internalTime: '2014-11-23T06:54:49',
+      jsDate: new Date(Date.UTC(2009,0,1,0,0,0) + 1000*185928889),
+      systemSeconds: 185957689,
+      systemTimeMsec: 1416725689000,
+    },
+    {
+      displayOffset: -28800,
+      internalTime: '2014-11-23T06:55:07',
+      jsDate: new Date(Date.UTC(2009,0,1,0,0,0) + 1000*185928907),
+      systemSeconds: 185957707,
+      systemTimeMsec: 1416725707000
+    },
+    {
+      displayOffset: -21644,
+      internalTime: '2014-12-25T21:34:45',
+      jsDate: new Date(Date.UTC(2009,0,1,0,0,0) + 1000*188753640),
+      systemSeconds: 188775285,
+      systemTimeMsec: 1419543285000
+    }
+  ];
+  _.each(settings, function(setting) {
+    setting.deviceTime = sundial.formatDeviceTime(setting.jsDate.toISOString());
+  });
+  it('is a function', function() {
+    expect(typeof userSettingsChanges).to.equal('function');
+  });
 
-	it('returns an object with `timeChanges` and `settingsChange` attributes', function() {
-		var res = userSettingsChanges([]);
-		expect(typeof res).to.equal('object');
-		expect(res.timeChanges).to.exist;
-		expect(res.settingChanges).to.exist;
-	});
+  it('returns an object with `timeChanges` and `settingsChange` attributes', function() {
+    var res = userSettingsChanges([]);
+    expect(typeof res).to.equal('object');
+    expect(res.timeChanges).to.exist;
+    expect(res.settingChanges).to.exist;
+  });
+
+  describe('timeChanges', function() {
+    var res = userSettingsChanges(settings, {builder: builder});
+
+    it('ignores records from 2009 when calculating `timeChanges`', function() {
+      _.each(res.timeChanges, function(change) {
+        expect(change.time.slice(0,4)).not.to.equal('2009');
+      });
+    });
+
+    it('only creates timeChange records when the displayOffset has changed', function() {
+      var res = userSettingsChanges(settings, {builder: builder});
+      var expectedChange = {
+        deviceTime: '2014-12-25T15:34:00',
+        change: {
+          agent: 'manual',
+          from: '2014-12-25T13:34:45',
+          to: '2014-12-25T15:34:01'
+        }
+      };
+      expect(res.timeChanges.length).to.equal(1);
+      expect(_.pick(res.timeChanges[0], ['deviceTime', 'change'])).to.deep.equal(expectedChange);
+    });
+  });
 });
