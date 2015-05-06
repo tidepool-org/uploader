33fadd29030e5dd4cd3f62622970d757f4cfbd8e
Jana E. Beck <jana.eliz.beck@gmail.com>
2015-05-06 16:16:34 -0700

    remove some missed references to MOCK mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
 var _ = require('lodash');
 
-if ((!process.env.API_URL || !process.env.UPLOAD_URL || !process.env.BLIP_URL) &&
-     process.env.MOCK != 'true') {
+if ((!process.env.API_URL || !process.env.UPLOAD_URL || !process.env.BLIP_URL)) {
   console.log('Using the default environment, which is now production.');
 } else {
   console.log('***** NOT using the default environment *****');
@@ -10,7 +9,6 @@ if ((!process.env.API_URL || !process.env.UPLOAD_URL || !process.env.BLIP_URL) &
   console.log('API_URL =', process.env.API_URL);
   console.log('UPLOAD_URL =', process.env.UPLOAD_URL);
   console.log('BLIP_URL =', process.env.BLIP_URL);
-  console.log('MOCK =', process.env.MOCK);
 }
 
 var config = {
@@ -25,18 +23,7 @@ var config = {
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.json$/, loader: 'json' }
     ]
-  },
-  resolve: {
-    alias: {
-      mock: './mock/empty.js'
-    }
   }
 };
-
-if (process.env.MOCK === 'true') {
-  config.resolve.alias = _.assign(config.resolve.alias, {
-    mock: './mock'
-  });
-}
 
 module.exports = config;
