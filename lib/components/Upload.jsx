632cd3434eabd3f30e5b3c4fc098d137b3275b5b
Gerrit Niezen <gerrit.niezen@gmail.com>
2015-09-07 11:28:30 +0100

    Added Windows USB driver link.

--- lib/components/Upload.jsx
+++ lib/components/Upload.jsx
@@ -43,8 +43,8 @@ var Upload = React.createClass({
       text: {
         VERIOIQ_NOT_SUPPORTED : 'VerioIQ upload is not currently supported on this operating system',
         VERIOIQ_SUPPORTED : 'Please download and install the USB device driver:',
-        WINDOWS_DRIVER_NAME: 'TODO',
-        WINDOWS_DRIVER_URL: 'TODO',
+        WINDOWS_DRIVER_NAME: 'TidepoolUSBDriverSetup.exe',
+        WINDOWS_DRIVER_URL: 'http://tidepool.org/downloads/',
         MAC_SILABS_DRIVER_NAME: 'MAC_OSX_VCP_Driver.zip',
         MAC_SILABS_DRIVER_URL: 'https://www.silabs.com/Support%20Documents/Software/Mac_OSX_VCP_Driver.zip',
         CARELINK_CREDS_NOT_SAVED :'Import from CareLink.<br>We will not store your credentials.',
