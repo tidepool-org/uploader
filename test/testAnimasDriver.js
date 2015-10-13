b44316a9255860f0d55604013e767291181e3c88
Gerrit Niezen <gerrit.niezen@gmail.com>
2015-10-13 14:57:45 +0100

    Stops reading when bolus packet is empty.

---
+++ test/testAnimasDriver.js
@@ -0,0 +1,5 @@
+
+//TODO:
+// Check if following (escaped with 0x7D) packet is parsed correctly
+// C0 80 74 44 49 30 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 8B 7D E1 C1
+// C0 80 7A 44 49 7D 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 38 7D E0 C1
