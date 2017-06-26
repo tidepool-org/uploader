d0025fa590f0a465944099ec031e1d780f3f888e
Chris McGee <krystophv@gmail.com>
2017-06-26 17:07:33 -0400

    exclude trackerjs loader for tests

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -4,13 +4,15 @@
 require('babel-register');
 const validate = require('webpack-validator');
 const devConfig = require('./webpack.config.development');
+const loaders = devConfig.default.module.loaders.slice(1);
+loaders.splice(-1);
 
 module.exports = validate({
   output: {
     libraryTarget: 'commonjs2'
   },
   module: {
-    // Use base + development loaders, but exclude 'babel-loader'
-    loaders: devConfig.default.module.loaders.slice(1)
+    // Use base + development loaders, but exclude 'babel-loader' and 'trackerjs'
+    loaders: loaders
   }
 });
