53b7c392375895ce13d80c031271560a092a948e
Lennart Goedhart <lennart@tidepool.org>
2017-08-02 09:46:35 +1000

    Use exact versions of npm modules Commit QA build version info

--- scripts/prepare-qa-build.js
+++ scripts/prepare-qa-build.js
@@ -1,4 +1,5 @@
 const shell = require('shelljs');
+const nodegit = require('nodegit');
 const {
   gitDescribeSync
 } = require('git-describe');
@@ -11,6 +12,21 @@ if (gitInfo.dirty) {
 
 const qaVersion = gitInfo.semverString.replace('+', '.');
 
-for (const file of ['package.json', 'app/package.json']) {
-  shell.sed('-i', '"version":[ ]*".*",$', `"version": "${qaVersion}",`, file);
-}
+(async() => {
+  const repo = await nodegit.Repository.open('.');
+  const index = await repo.refreshIndex();
+
+  const defaultSig = nodegit.Signature.default(repo);
+
+  for (const file of ['package.json', 'app/package.json']) {
+    shell.sed('-i', '"version":[ ]*".*",$', `"version": "${qaVersion}",`, file);
+    await index.addByPath(file);
+  }
+
+  await index.write();
+  const oid = await index.writeTree();
+  const head = await nodegit.Reference.nameToId(repo, 'HEAD');
+  const parent = await repo.getCommit(head);
+  const commitId = await repo.createCommit('HEAD', defaultSig, defaultSig, `Create QA build version ${qaVersion}`, oid, [parent]);
+  console.log(`Created QA build version '${qaVersion}' with commit ID '${commitId}'`);
+})();
