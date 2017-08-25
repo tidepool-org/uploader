bca3251776be39818d1f6b92860a8108209a8f6d
Gerrit Niezen <gerrit.niezen@gmail.com>
2017-08-25 11:51:07 +0100

    change  to

--- resources/mac/updateDrivers.sh
+++ resources/mac/updateDrivers.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+DRIVER_PATH=$1
 
-echo "Driver path: $1"
+echo "Driver path: $DRIVER_PATH"
 echo "Unloading and uninstalling old extensions..."
 # unload old extensions
 kextunload /Library/Extensions/sweetspot.DexComUSB.kext
@@ -16,15 +17,15 @@ rm -rf /Library/Extensions/ProlificUsbSerial.kext
 
 # install new extensions
 echo "Installing and loading new extensions..."
-cp -R "$1/DexComUSB.kext" /Library/Extensions/DexComUSB.kext
+cp -R "$DRIVER_PATH/DexComUSB.kext" /Library/Extensions/DexComUSB.kext
 chown -R root:wheel /Library/Extensions/DexcomUSB.kext
 kextload /Library/Extensions/DexComUSB.kext/
 
-cp -R "$1/SiLabsUSBDriver.kext" /Library/Extensions/SiLabsUSBDriver.kext
+cp -R "$DRIVER_PATH/SiLabsUSBDriver.kext" /Library/Extensions/SiLabsUSBDriver.kext
 chown -R root:wheel /Library/Extensions/SiLabsUSBDriver.kext
 kextload /Library/Extensions/SiLabsUSBDriver.kext/
 
-cp -R "$1/ProlificUsbSerial.kext" /Library/Extensions/ProlificUsbSerial.kext
+cp -R "$DRIVER_PATH/ProlificUsbSerial.kext" /Library/Extensions/ProlificUsbSerial.kext
 chown -R root:wheel /Library/Extensions/ProlificUsbSerial.kext
 kextload /Library/Extensions/ProlificUsbSerial.kext/
 
