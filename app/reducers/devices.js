47626f1ca06c670b0f991c2b8b1bb692dae1265b
Gerrit Niezen <gerrit.niezen@gmail.com>
2017-09-06 15:58:56 +0100

    move and combine driver manifests into devices.js

--- app/reducers/devices.js
+++ app/reducers/devices.js
@@ -22,91 +22,114 @@ const devices = {
     name: 'Medtronic - 523, 723 or 530G',
     selectName: 'Medtronic direct from Bayer Contour Next Link',
     source: {type: 'device', driverId: 'Medtronic'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    usb: [
+      {vendorId: 6777, productId: 25344}, // Bayer Contour Next Link mmol/L
+      {vendorId: 6777, productId: 25088}  // Bayer Contour Next Link
+    ]
   },
   omnipod: {
     instructions: 'Plug in PDM with mini-USB and choose .ibf file from PDM',
     key: 'omnipod',
     name: 'Insulet OmniPod',
     source: {type: 'block', driverId: 'InsuletOmniPod', extension: '.ibf'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'block',
+    usb: [
+      {vendorId: 7734, productId: 2}
+    ]
   },
   dexcom: {
     instructions: 'Plug in receiver with micro-USB',
     key: 'dexcom',
     name: 'Dexcom',
     source: {type: 'device', driverId: 'Dexcom'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    usb: [
+      {vendorId: 8867, productId: 71}
+    ]
   },
   precisionxtra: {
     instructions: 'Plug in meter with cable',
     key: 'precisionxtra',
     name: 'Abbott Precision Xtra',
     source: {type: 'device', driverId: 'AbbottPrecisionXtra'},
-    enabled: {mac: false, win: true}
+    enabled: {mac: false, win: true},
+    mode: 'serial',
+    usb: [
+      {vendorId: 6753, productId: 13344}
+    ]
   },
   tandem: {
     instructions: 'Plug in pump with micro-USB',
     key: 'tandem',
     name: 'Tandem',
     source: {type: 'device', driverId: 'Tandem'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    bitrate: 921600,
+    sendTimeout: 50,
+    receiveTimeout: 50,
+    usb: [
+      {vendorId: 1155, productId: 22336}
+    ]
   },
   abbottfreestylelite: {
     instructions: 'Plug in meter with cable',
     key: 'abbottfreestylelite',
-    name: 'Abbott FreeStyle Lite',
+    name: 'Abbott FreeStyle Lite & Freedom Lite',
     source: {type: 'device', driverId: 'AbbottFreeStyleLite'},
-    enabled: {mac: false, win: true}
-  },
-  abbottfreestylefreedomlite: {
-    instructions: 'Plug in meter with cable',
-    key: 'abbottfreestylefreedomlite',
-    name: 'Abbott FreeStyle Freedom Lite',
-    source: {type: 'device', driverId: 'AbbottFreeStyleFreedomLite'},
-    enabled: {mac: false, win: true}
+    enabled: {mac: false, win: true},
+    bitrate: 19200,
+    mode: 'serial',
+    usb: [
+      {vendorId: 6753, productId: 13328}, // Abbott cable
+      {vendorId: 1027, productId: 24577}  // FTDI cable
+    ]
   },
   bayercontournext: {
-    instructions: 'Plug in meter with micro-USB',
+    instructions: 'Plug meter into USB port',
     key: 'bayercontournext',
     name: 'Bayer Contour Next',
     source: {type: 'device', driverId: 'BayerContourNext'},
-    enabled: {mac: true, win: true}
-  },
-  bayercontournextusb: {
-    instructions: 'Plug meter into USB port',
-    key: 'bayercontournextusb',
-    name: 'Bayer Contour Next USB',
-    source: {type: 'device', driverId: 'BayerContourNextUsb'},
-    enabled: {mac: true, win: true}
-  },
-  bayercontourusb: {
-    instructions: 'Plug meter into USB port',
-    key: 'bayercontourusb',
-    name: 'Bayer Contour USB',
-    source: {type: 'device', driverId: 'BayerContourUsb'},
-    enabled: {mac: true, win: true}
-  },
-  bayercontournextlink: {
-    instructions: 'Plug meter into USB port',
-    key: 'bayercontournextlink',
-    name: 'Bayer Contour Next Link',
-    source: {type: 'device', driverId: 'BayerContourNextLink'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'HID',
+    usb: [
+      {vendorId: 6777, productId: 29520}, // Bayer Contour Next
+      {vendorId: 6777, productId: 29712}, // Bayer Contour Next USB
+      {vendorId: 6777, productId: 25088}, // Bayer Contour Next Link
+      {vendorId: 6777, productId: 25344}, // Bayer Contour Next Link mmol/L
+      {vendorId: 6777, productId: 25104}, // Bayer Contour Next Link 2.4
+      {vendorId: 6777, productId: 24578}  // Bayer Contour USB
+    ]
   },
   animas: {
     instructions: 'Suspend and align back of pump with IR dongle front',
     key: 'animas',
     name: 'Animas',
     source: {type: 'device', driverId: 'Animas'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    bitrate: 9600,
+    ctsFlowControl: true,
+    sendTimeout: 500,
+    receiveTimeout: 500,
+    usb: [
+      {vendorId: 1659, productId: 8963}
+    ]
   },
   onetouchverioiq: {
     instructions: 'Plug in meter with mini-USB',
     name: 'OneTouch VerioIQ',
     key: 'onetouchverioiq',
     source: {type: 'device', driverId: 'OneTouchVerioIQ'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    bitrate: 38400,
+    usb: [
+      {vendorId: 4292, productId: 34215}
+    ]
   },
   /* TODO: re-enable these after Electron is on production
   onetouchultramini: {
@@ -115,7 +138,11 @@ const devices = {
     key: 'onetouchultramini',
     showDriverLink: {mac: true, win: true},
     source: {type: 'device', driverId: 'OneTouchUltraMini'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    usb: [
+      { vendorId: 1027, productId: 24577 }
+    ]
   },
   onetouchultra2: {
     instructions: 'Plug in meter with cable',
@@ -123,7 +150,14 @@ const devices = {
     key: 'onetouchultra2',
     showDriverLink: {mac: true, win: true},
     source: {type: 'device', driverId: 'OneTouchUltra2'},
-    enabled: {mac: true, win: true}
+    enabled: {mac: true, win: true},
+    mode: 'serial',
+    bitrate: 9600,
+    sendTimeout: 5000,
+    receiveTimeout: 5000,
+    usb: [
+      {vendorId: 1027, productId: 24577}
+    ]
   }
   */
 };
