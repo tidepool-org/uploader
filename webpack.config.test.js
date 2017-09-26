06b49215ed1d3acd87fccbf8023d31fa456cacf8
Chris McGee <krystophv@gmail.com>
2017-09-26 10:59:36 -0400

    revert changes to test config

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -4,14 +4,13 @@
 require('babel-register');
 const validate = require('webpack-validator');
 const devConfig = require('./webpack.config.development');
-const loaders = devConfig.default.module.loaders.slice(1);
 
 module.exports = validate({
   output: {
     libraryTarget: 'commonjs2'
   },
   module: {
-    // Use base + development loaders, but exclude 'babel-loader' and 'trackerjs'
-    loaders: loaders
+    // Use base + development loaders, but exclude 'babel-loader'
+    loaders: devConfig.default.module.loaders.slice(1)
   }
 });
