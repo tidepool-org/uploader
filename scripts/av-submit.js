4d3a905c6a04a8b084a263d226d7bf558a55ed73
Gerrit Niezen <gerrit.niezen@gmail.com>
2019-03-15 17:08:28 +0000

    script now sends an templated e-mail to McAfee

--- scripts/av-submit.js
+++ scripts/av-submit.js
@@ -25,14 +25,14 @@ const fs = require('fs');
 const builder = require('xmlbuilder');
 const Client = require('ftp');
 const https = require('https');
+const aws = require('aws-sdk');
 
 const ORG = 'tidepool-org';
 const REPO = 'chrome-uploader';
+const CONTACT_PERSON = 'Gerrit Niezen';
+const CONTACT_EMAIL = 'gerrit@tidepool.org';
 
-if(!process.env.FTP_AV_PASSWORD_TIDEPOOL) {
-  console.log('Please set the FTP_AV_PASSWORD_TIDEPOOL environment variable.');
-} else {
-
+function getDownloadURL(cb) {
   const options = {
     hostname: 'api.github.com',
     port: 443,
@@ -54,42 +54,102 @@ if(!process.env.FTP_AV_PASSWORD_TIDEPOOL) {
     res.on('end', function () {
       const data = JSON.parse(body);
 
-      let downloadURL = '';
+      let windows = '';
+      let macOS = '';
+
       for (let asset of data.assets) {
         if (asset.name.endsWith('.exe')) {
-          downloadURL = asset.browser_download_url;
+          windows = asset.browser_download_url;
+        }
+
+        if (asset.name.endsWith('.dmg')) {
+          macOS = asset.browser_download_url;
         }
       }
 
-      console.log('File URL:', downloadURL);
-
-      const xml = builder.create('products', { encoding: 'utf-8'})
-        .att('xmlns', 'http://www.kaspersky.com/KLSRL/ISV')
-        .ele('product')
-          .ele('url', downloadURL)
-        .end({ pretty: true});
-
-      console.log(xml);
-
-      fs.writeFile('uploader.xml', xml, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-
-        const c = new Client();
-        c.on('ready', function() {
-          c.put('uploader.xml', 'uploader.xml', function(err) {
-            if (err) throw err;
-            c.end();
-            console.log('Uploaded file to FTP server.');
-          });
-        });
-
-        c.connect({
-          host : 'whitelist1.kaspersky-labs.com',
-          user : 'wl-Tidepool',
-          password: process.env.FTP_AV_PASSWORD_TIDEPOOL
-        });
-      });
+      console.log('Windows Download URL:', windows);
+      console.log('macOS Download URL:', macOS);
+      return cb(null, { windows, macOS });
     });
+  }).on('error', (e) => {
+    console.error(e);
+    return cb(e);
   });
 }
+
+function sendToKaspersky(downloadURL) {
+  if(!process.env.FTP_AV_PASSWORD_TIDEPOOL) {
+    console.log('Please set the FTP_AV_PASSWORD_TIDEPOOL environment variable');
+  } else {
+
+    const xml = builder.create('products', { encoding: 'utf-8'})
+      .att('xmlns', 'http://www.kaspersky.com/KLSRL/ISV')
+      .ele('product')
+        .ele('url', downloadURL.windows)
+      .end({ pretty: true});
+
+    console.log(xml);
+
+    fs.writeFile('uploader.xml', xml, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+
+      const c = new Client();
+      c.on('ready', function() {
+        c.put('uploader.xml', 'uploader.xml', function(err) {
+          if (err) throw err;
+          c.end();
+          console.log('Uploaded file to FTP server.');
+        });
+      });
+
+      c.connect({
+        host : 'whitelist1.kaspersky-labs.com',
+        user : 'wl-Tidepool',
+        password: process.env.FTP_AV_PASSWORD_TIDEPOOL
+      });
+    });
+  }
+}
+
+function sendtoMcAfee(downloadURL) {
+  aws.config.update({region: 'us-west-2'});
+
+  var params = {
+    Destination: { /* required */
+      CcAddresses: [
+        CONTACT_EMAIL,
+      ],
+      ToAddresses: [
+        'gerrit.niezen@gmail.com',
+      ]
+    },
+    Source: 'noreply@tidepool.org', /* required */
+    Template: 'mcafee-template', /* required */
+    ConfigurationSetName: 'mcafee-email',
+    TemplateData: '{ \"contactName\":\"' + CONTACT_PERSON + '\", \"windowsLink\":\"' + downloadURL.windows + '\", \"macOSLink\":\"' + downloadURL.macOS + '\"}', /* required */
+    ReplyToAddresses: [
+       'noreply@tidepool.org',
+       CONTACT_EMAIL,
+    ],
+  };
+
+  // Create the promise and SES service object
+  const sendPromise = new aws.SES({apiVersion: '2010-12-01'})
+                             .sendTemplatedEmail(params).promise();
+
+  // Handle promise's fulfilled/rejected states
+  sendPromise.then(
+    function(data) {
+      console.log(data);
+    }).catch(
+      function(err) {
+      console.error(err, err.stack);
+    });
+
+  }
+
+getDownloadURL((error, downloadURL) => {
+  sendtoMcAfee(downloadURL);
+  //TODO: sendToKaspersky(downloadURL);
+});
