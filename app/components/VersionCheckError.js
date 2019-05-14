cd955e2cc9ffbd5705d8f2cdeee36a73878b5a71
Chris McGee <krystophv@gmail.com>
2019-05-14 12:43:16 -0400

    modal styling updates

--- app/components/VersionCheckError.js
+++ app/components/VersionCheckError.js
@@ -1,19 +1,19 @@
 /*
-* == BSD2 LICENSE ==
-* Copyright (c) 2016, Tidepool Project
-*
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the associated License, which is identical to the BSD 2-Clause
-* License as published by the Open Source Initiative at opensource.org.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE. See the License for more details.
-*
-* You should have received a copy of the License along with this program; if
-* not, you can obtain one from Tidepool Project at tidepool.org.
-* == BSD2 LICENSE ==
-*/
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2016, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
 
 import cx from 'classnames';
 import PropTypes from 'prop-types';
@@ -22,6 +22,7 @@ import React, { Component } from 'react';
 import errorText from '../constants/errors';
 
 import styles from '../../styles/components/VersionCheck.module.less';
+import CloudOff from '@material-ui/icons/CloudOff';
 
 export default class VersionCheckError extends Component {
   static propTypes = {
@@ -34,8 +35,8 @@ export default class VersionCheckError extends Component {
       CONNECT: 'Please check your connection, quit & relaunch to try again.',
       ERROR_DETAILS: 'Details for Tidepool\'s developers:',
       OFFLINE: 'You\'re not connected to the Internet.',
-      SERVERS_DOWN: 'Tidepool\'s servers are down.',
-      TRY_AGAIN: 'In a few minutes, please quit & relaunch to try again.'
+      SERVERS_DOWN: 'We can\'t connect to Tidepool right now.',
+      TRY_AGAIN: 'Quit & relaunch the Uploader to try again.'
     }
   };
 
@@ -44,28 +45,38 @@ export default class VersionCheckError extends Component {
   }
 
   render() {
-    const { errorMessage } = this.props;
+    const { errorMessage } = this.props; 
     const userErrorText = this.props.errorText;
-    const offline = (errorMessage === errorText.E_OFFLINE);
+    const offline = errorMessage === errorText.E_OFFLINE;
     const errorDetails = offline ? null : (
       <div className={styles.error}>
         <p className={styles.errorText}>{userErrorText.ERROR_DETAILS}</p>
         <p className={styles.errorText}>{errorMessage}</p>
       </div>
     );
-    const firstLine = offline ? userErrorText.OFFLINE : userErrorText.SERVERS_DOWN;
-    const secondLine = offline ? userErrorText.CONNECT : userErrorText.TRY_AGAIN;
+    const firstLine = offline
+      ? userErrorText.OFFLINE
+      : userErrorText.SERVERS_DOWN;
+    const secondLine = offline
+      ? userErrorText.CONNECT
+      : userErrorText.TRY_AGAIN;
     const versionCheckClass = cx({
       [styles.failed]: !offline,
       [styles.offline]: offline
     });
+
     return (
-      <div className={versionCheckClass}>
-        <div className={styles.text}>
-          <p className={styles.paragraph}>{firstLine}</p>
-          <p className={styles.paragraph}>{secondLine}</p>
+      <div className={styles.modalWrap}>
+        <div className={styles.modal}>
+          <div className={versionCheckClass}>
+            <div className={styles.text}>
+              <CloudOff classes={{root: styles.icon}}/>
+              <p className={styles.lineOne}>{firstLine}</p>
+              <p className={styles.lineTwo}>{secondLine}</p>
+            </div>
+            {errorDetails}
+          </div>
         </div>
-        {errorDetails}
       </div>
     );
   }
