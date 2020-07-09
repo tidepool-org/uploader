32805db2441b0441e505607b8f84c3f91a4b0e12
Gerrit Niezen <gerrit@tidepool.org>
2020-07-09 14:16:35 +0100

    add listing for ReliOn meters

--- app/reducers/devices.js
+++ app/reducers/devices.js
@@ -33,6 +33,13 @@ const devices = {
     source: {type: 'device', driverId: 'CareSens'},
     enabled: {mac: true, win: true, linux: true}
   },
+  relion: {
+    instructions: 'Plug in meter with cable and make sure the meter is switched on',
+    name: 'ReliOn Prime, Confirm & Premier (BLU, Voice & Classic)',
+    key: 'relion',
+    source: {type: 'device', driverId: 'CareSens'},
+    enabled: {mac: true, win: true, linux: true}
+  },
   medtronic: {
     instructions: 'Connect your Contour Next Link to your computer',
     image: {
