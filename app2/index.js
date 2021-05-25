929cfcd9f97c9a99021d8c8073d2c14e001a69f6
Gerrit Niezen <gerrit@tidepool.org>
2021-05-25 13:37:44 +0100

    web2 uploads successfully

--- app2/index.js
+++ app2/index.js
@@ -4,7 +4,9 @@ import driverManifests from '../lib/core/driverManifests';
 import api from '../lib/core/api';
 import builder from '../lib/objectBuilder';
 
-let button = document.getElementById('connect');
+const button = document.getElementById('connect');
+const login = document.getElementById('login');
+const app = document.getElementById('app');
 
 const driverId = 'BayerContourNext';
 const driverManifest = _.get(driverManifests, driverId);
@@ -16,10 +18,49 @@ const filters = driverManifest.usb.map(({vendorId, productId}) => ({
 
 const options = {
   api,
-  // timezone: program.timezone,
-  // version: `${pkg.version}`,
+  timezone: 'Europe/London',
+  version: 'uploader web2',
   builder: builder(),
 };
+
+const config = {
+  API_URL: 'https://qa2.development.tidepool.org',
+  UPLOAD_URL: 'https://qa2.development.tidepool.org',
+  DATA_URL: 'https://qa2.development.tidepool.org/dataservices',
+  BLIP_URL: 'https://app-qa2.development.tidepool.org'
+};
+
+api.create({
+  apiUrl: config.API_URL,
+  uploadUrl: config.UPLOAD_URL,
+  dataUrl: config.DATA_URL,
+  version: 'uploader web2',
+});
+
+login.addEventListener('submit', (event) => {
+  const username = login.elements['username'].value;
+  const password = login.elements['password'].value;
+
+  api.init(() => {
+    api.user.login({
+        username,
+        password
+      }, (error, loginData) => {
+      if (error) {
+        console.log(error);
+      } else {
+        options.targetId = loginData.userid;
+        options.groupId = loginData.userid;
+
+        login.setAttribute('hidden', '');
+        app.removeAttribute('hidden');
+      }
+    });
+  });
+
+  event.preventDefault();
+});
+
 
 button.addEventListener('click', async() => {
   try {
