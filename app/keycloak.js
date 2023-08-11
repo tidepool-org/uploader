472a1842464d353c4d18660ad827b45a4f816b5b
Chris McGee <chris@tidepool.org>
2023-08-10 22:07:39 -0400

    [UPLOAD-1123] only process hash changes once, more judicial logging

--- app/keycloak.js
+++ app/keycloak.js
@@ -42,7 +42,10 @@ const updateKeycloakConfig = (info, store) => {
   }
 };
 
+let latestKeycloakEvent = null;
+
 const onKeycloakEvent = (store) => (event, error) => {
+  latestKeycloakEvent = event;
   switch (event) {
     case 'onReady': {
       let logoutUrl = keycloak.createLogoutUrl({
@@ -116,7 +119,7 @@ const onKeycloakTokens = (store) => (tokens) => {
       if (tokens.token !== keycloak?.token) {
         if (rollbar) {
           rollbar.info('keycloak token mismatch', {
-            keycloakToken: jose.decodeJwt(keycloak?.token),
+            keycloakToken: keycloak?.token ? jose.decodeJwt(keycloak?.token) : {},
             tokensToken: tokenParsed,
           });
         }
@@ -137,6 +140,17 @@ const onKeycloakTokens = (store) => (tokens) => {
       return;
     }
   } else {
+    const expectedEvents = [
+      'onReady',
+      'onAuthLogout',
+      'onAuthRefreshError',
+      'onAuthError',
+      'onInitError'
+    ];
+    if (expectedEvents.includes(latestKeycloakEvent)) {
+      return;
+    }
+
     // if we don't have a token, we can't save the session
     if(rollbar) {
       rollbar.error('keycloak token missing', {
@@ -205,6 +219,8 @@ export const keycloakMiddleware = (api) => (storeAPI) => (next) => (action) => {
   return next(action);
 };
 
+let hashChanges = [];
+
 let keyCount = 0;
 
 export const KeycloakWrapper = (props) => {
@@ -234,13 +250,18 @@ export const KeycloakWrapper = (props) => {
   // incrementing externally defined `key` forces unmount/remount as provider doesn't expect to
   // have the authClient refreshed and only sets up refresh timeout on mount
   const onHashChange = useCallback(() => {
-    keycloak = new Keycloak({
-      url: keycloakConfig.url,
-      realm: keycloakConfig.realm,
-      clientId: 'tidepool-uploader-sso',
-    });
-    keyCount++;
-    forceUpdate();
+    // we only want to do this once per hash since people can hit the launch button multiple times
+    if (!hashChanges.includes(window.location.hash)) {
+      hashChanges.push(window.location.hash);
+
+      keycloak = new Keycloak({
+        url: keycloakConfig.url,
+        realm: keycloakConfig.realm,
+        clientId: 'tidepool-uploader-sso',
+      });
+      keyCount++;
+      forceUpdate();
+    }
   }, [keycloakConfig.realm, keycloakConfig.url, blipRedirect]);
 
   useEffect(() => {
@@ -249,6 +270,16 @@ export const KeycloakWrapper = (props) => {
       window.removeEventListener('hashchange', onHashChange, false);
     };
   }, [onHashChange]);
+
+  // clear the refresh timeout on unmount
+  useEffect(() => {
+    return () => {
+      if (refreshTimeout) {
+        clearTimeout(refreshTimeout);
+        refreshTimeout = null;
+      }
+    };
+  }, []);
 
   if (keycloakConfig.url && keycloakConfig.instantiated && blipRedirect) {
     return (
