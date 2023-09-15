5b7305619c0c0c32dc5402a48176b688bef6b144
Chris McGee <chris@tidepool.org>
2023-09-15 15:52:01 -0400

    [UPLOAD-1068] trying a circleCI build

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-wget -q -O artifact_node.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact.sh'
+wget -q -O artifact_node.sh 'https://raw.githubusercontent.com/tidepool-org/tools/add-circleci/artifact/artifact.sh'
 chmod +x artifact_node.sh
 
 . ./version.sh
