97ac6d316eccec5bdb15e390d3cff512557644e8
Gerrit Niezen <gerrit@tidepool.org>
2024-02-08 13:35:36 +0000

    use require as it's node

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,4 +1,4 @@
-import { notarize } from '@electron/notarize';
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
