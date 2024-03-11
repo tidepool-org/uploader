eb139467dd887364df404e68881831d7a4c1fb27
Gerrit Niezen <gerrit@tidepool.org>
2024-03-11 11:07:12 +0000

    successfully finds volume using vendor ID

--- extension/verio-helper/helper.zig
+++ extension/verio-helper/helper.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 const windows = @import("std").os.windows;
-const W = std.unicode.utf8ToUtf16LeStringLiteral;
+const MAX_PATH = windows.MAX_PATH + 1;
+
+extern "kernel32" fn FindFirstVolumeW(lpBuffer: [*]u16, bufferLength: u32) ?windows.HANDLE;
+extern "kernel32" fn FindNextVolumeW(hFindVolume: windows.HANDLE, lpBuffer: [*]u16, bufferLength: u32) bool;
+extern "kernel32" fn FindVolumeClose(hFindVolume: windows.HANDLE) bool;
+extern "kernel32" fn GetVolumePathNamesForVolumeNameW(volumeName: [*]const u16, volumePathNames: [*]u16, bufferLength: u32, returnLength: *u32) bool;
+
 const Allocator = std.mem.allocator;
 
 const OpenError = error {
@@ -9,13 +15,155 @@ const OpenError = error {
 	ReadFailed,	
 };
 
-fn openDevice() !windows.HANDLE {
-    const devicePath = "\\\\.\\E:"; // Change C: to your target volume
-    const FSCTL_LOCK_VOLUME = 0x00090018;
+const FindError = error {
+	FirstFailed,
+	ConvertFailed,
+	FindFailed,
+};
 
+fn findDevice() ![:0]u16 {
+	const IOCTL_STORAGE_QUERY_PROPERTY = 0x2D1400;
+
+	const STORAGE_DEVICE_DESCRIPTOR = extern struct {
+	    Version: u32,
+	    Size: u32,
+	    DeviceType: u8,
+	    DeviceTypeModifier: u8,
+	    RemovableMedia: bool,
+	    CommandQueueing: bool,
+	    VendorIdOffset: u32,
+	    ProductIdOffset: u32,
+	    ProductRevisionOffset: u32,
+	    SerialNumberOffset: u32,
+	    BusType: u8,
+	    RawPropertiesLength: u32,
+	    RawDeviceProperties: [1]u8
+	};
+
+	var volume_name_buffer: [MAX_PATH]u16 = undefined;
+    var volume_path_names_buffer: [MAX_PATH]u16 = undefined;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const printAllocator = gpa.allocator();
+
+    const find_volume_handle = FindFirstVolumeW(&volume_name_buffer, volume_name_buffer.len) orelse {
+        try sendReply("error", "Failed to find first volume");
+        return FindError.FirstFailed;
+    };
+
+    defer _ = FindVolumeClose(find_volume_handle);
+
+    while (true) {
+        // Convert volume name from UTF-16 to UTF-8 for printing
+        const volume_name = std.unicode.utf16leToUtf8Alloc(std.heap.page_allocator, volume_name_buffer[0..]) catch {
+            try sendReply("error", "Failed to convert volume name to UTF-8");
+            return FindError.ConvertFailed;
+        };
+        std.debug.print("Volume Name: {s}\n", .{std.mem.sliceTo(volume_name[0..], 0)});
+
+        // Get volume path names
+        var return_length: u32 = 0;
+        if (GetVolumePathNamesForVolumeNameW(&volume_name_buffer, &volume_path_names_buffer, volume_path_names_buffer.len, &return_length)) {
+            const volume_path_names = try std.unicode.utf16leToUtf8Alloc(std.heap.page_allocator, volume_path_names_buffer[0..]);
+
+            const path = std.mem.sliceTo(volume_path_names, 0);
+            std.debug.print("Volume Path Names: {s}\n", .{path});
+
+            if (path.len == 0) {
+            	if (!FindNextVolumeW(find_volume_handle, &volume_name_buffer, volume_name_buffer.len)) {
+            		break;
+            	}
+            	continue;
+            }
+
+            const devicePath = try std.fmt.allocPrint(std.heap.page_allocator, "\\\\.\\{s}", .{path[0..2]});
+            std.debug.print("Device Path: {s}\n", .{devicePath});
+
+			const path_utf16 = try std.unicode.utf8ToUtf16LeWithNull(std.heap.page_allocator, devicePath);
+
+	        const volumeHandle = windows.kernel32.CreateFileW(
+	        	path_utf16,
+	        	windows.GENERIC_READ,
+	        	windows.FILE_SHARE_READ | windows.FILE_SHARE_WRITE,
+	        	null,
+	        	windows.OPEN_EXISTING,
+	        	windows.FILE_ATTRIBUTE_NORMAL,
+	        	null
+	        );
+
+	        if (volumeHandle == windows.INVALID_HANDLE_VALUE) {
+	        	std.log.err("Failed to open volume", .{});
+	        	if (!FindNextVolumeW(find_volume_handle, &volume_name_buffer, volume_name_buffer.len)) {
+		        	break;
+	        	}
+	        	continue;
+	        }
+			defer windows.CloseHandle(volumeHandle);
+
+			const StoragePropertyQuery  = packed struct {
+				propertyId: u32,
+				queryType: u32,
+				parameters: u8 = undefined
+			};
+			
+			const spq = StoragePropertyQuery {
+				.propertyId = 0,
+				.queryType = 0
+			};
+			var deviceDescriptor: [1024]u8 = undefined;
+	        
+	        windows.DeviceIoControl(
+	        	volumeHandle,
+	        	IOCTL_STORAGE_QUERY_PROPERTY,
+	        	std.mem.asBytes(&spq),
+	        	&deviceDescriptor,
+	        ) catch {
+	        	std.log.err("Storage query failed", .{});
+	        	continue;
+	        };
+
+	        const descriptor = std.mem.bytesAsSlice(STORAGE_DEVICE_DESCRIPTOR, deviceDescriptor[0..@sizeOf(STORAGE_DEVICE_DESCRIPTOR)]);
+		std.debug.print("VendorIdOffset: {d}\n", .{descriptor[0].VendorIdOffset});
+		std.debug.print("Size: {d}\n", .{descriptor[0].Size});
+
+		const offset = descriptor[0].VendorIdOffset;
+		const vendorID = std.mem.sliceTo(deviceDescriptor[offset..], 0);
+
+		const slice = try std.fmt.allocPrint(printAllocator, "Vendor ID: {s}\n", .{vendorID});
+		defer printAllocator.free(slice);
+		try sendReply("info", slice);
+
+		if (std.mem.eql(u8, vendorID, "LifeScan")) {
+			return path_utf16;
+		}
+		
+
+
+        } else {
+            std.log.err("Failed to get volume path names", .{});
+        }
+
+        if (!FindNextVolumeW(find_volume_handle, &volume_name_buffer, volume_name_buffer.len)) {
+            break;
+        }
+    }
+
+    try sendReply("error", "Could not find device");
+    return FindError.FindFailed;
+
+}
+
+fn openDevice() !windows.HANDLE {
+    const FSCTL_LOCK_VOLUME = 0x00090018;
+    
+    const devicePath = findDevice() catch |err| {
+    	try sendReply("error", "Could not find device");
+	return err;
+    };
+    
     // Open the volume
     const handle = windows.kernel32.CreateFileW(
-	W(devicePath),
+	devicePath,
 	windows.GENERIC_READ | windows.GENERIC_WRITE,
 	windows.FILE_SHARE_READ | windows.FILE_SHARE_WRITE,
 	null,
@@ -35,7 +183,7 @@ fn openDevice() !windows.HANDLE {
         null,
         null,
     ) catch {
-		try sendReply("error", "Failed to lock volume");
+	try sendReply("error", "Failed to lock volume");
         windows.CloseHandle(handle);
         return OpenError.LockFailed;
     };
