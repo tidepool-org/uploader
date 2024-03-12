feb080f11e1dc47951af0342bfca07a0c489c7b7
Gerrit Niezen <gerrit@tidepool.org>
2024-03-12 14:36:39 +0000

    move helper to separate rep; get version from native app

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2024, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ */
 
 let reply;
 let port;
@@ -5,7 +19,7 @@ let port;
 /* global chrome */
 
 function connect() {
-    const hostName = 'org.tidepool.uploader';
+    const hostName = 'org.tidepool.uploader-helper';
     console.log('Connecting to native messaging host', hostName);
     port = chrome.runtime.connectNative(hostName);
     port.onMessage.addListener(onNativeMessage);
@@ -44,7 +58,7 @@ chrome.runtime.onMessageExternal.addListener(
 	        sendNativeMessage(request);
 
 	        reply = sendResponse;
-	        return true; // indicates you will asynchronously use sendResponse
+	        return true; // indicates we will asynchronously use sendResponse
 	    } else {
 	    	sendResponse({ msgType: 'error', details: 'Not connected.'});
 	    }
