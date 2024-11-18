eaf7dbf52ab67ee10d63f072586a5eb01afd8d0c
Gerrit Niezen <gerrit@tidepool.org>
2024-11-18 16:45:04 +0000

    finish rest of updates

--- app/main.dev.js
+++ app/main.dev.js
@@ -71,7 +71,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')(); // eslint-disable-line global-require
   const p = path.join(__dirname, '..', 'app', 'node_modules'); // eslint-disable-line
   require('module').globalPaths.push(p); // eslint-disable-line
   process.env.APPIMAGE = path.join(__dirname, 'release');
