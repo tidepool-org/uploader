d484c8ba1f8823dd96523f5eb02f1d0220c9ac59
Gerrit Niezen <gerrit@tidepool.org>
2025-07-07 16:48:07 +0100

    use process.env to get at env vars

--- customSign.js
+++ customSign.js
@@ -6,14 +6,30 @@ function isEmpty(value) {
 
 // uses configuration.path to sign the file, passes in the keyvault auth as an env variable
 exports.default = async function (configuration) {
+  const timeserver = 'http://timestamp.digicert.com';
+  const azureURL = process.env.AZURE_KEY_VAULT_URI;
+  const certificateName = process.env.AZURE_CERT_NAME;
+
   if (isEmpty(configuration.path)) {
     throw new Error('Path to file is required');
   }
-  // AzureSignTool command with all the required parameters
-  const command = 'azuresigntool sign -kvu "${AZURE_KEY_VAULT_URI}" --azure-key-vault-managed-identity -kvc "${AZURE_CERT_NAME}" -tr http://timestamp.digicert.com -v';
 
-  // throws an error if non-0 exit code, that's what we want.
-  cp.execSync(`${command} "${configuration.path}"`, {
-    stdio: 'inherit',
-  });
+  const command = [
+      'azuresigntool.exe sign',
+      '-kvu',
+      azureURL,
+      '--azure-key-vault-managed-identity',
+      '-kvc',
+      certificateName,
+      '-tr',
+      timeserver,
+      //'-td',
+      //'sha384',
+      '-v',
+    ];
+
+    // throws an error if non-0 exit code, that's what we want.
+    cp.execSync(`${command.join(' ')} "${configuration.path}"`, {
+      stdio: 'inherit',
+    });
 };
