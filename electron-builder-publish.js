d6f9f104d2583f004382526863013a986f0d2c8c
Gerrit Niezen <gerrit@tidepool.org>
2025-09-23 16:29:08 +0100

    add electron-builder to asarunpack

--- electron-builder-publish.js
+++ electron-builder-publish.js
@@ -96,6 +96,7 @@ const config = {
     asarUnpack: [
       'app/node_modules/keytar', // https://github.com/electron-userland/electron-builder/issues/3940#issuecomment-900527250
       'app/node_modules/@tidepool/direct-io',
+      'node_modules/electron-builder'
     ],
   },
   protocols: [{
