1cc0e6f3be3ff50579ee61dae057eb9c340fa9d2
Gerrit Niezen <gerrit@tidepool.org>
2026-07-21 16:54:17 +0100

    update deps

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,6 @@ const developmentEnvironments = ['development', 'test'];
 const developmentPlugins = [require('react-hot-loader/babel')];
 
 const productionPlugins = [
-  require('babel-plugin-dev-expression'),
-
   // babel-preset-react-optimize
   require('@babel/plugin-transform-react-constant-elements'),
   require('@babel/plugin-transform-react-inline-elements'),
